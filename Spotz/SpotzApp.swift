e563682ecdc1560a6f7c03fc5493c738805d954c
FullMetalFist <m.vilabrera@gmail.com>
2024-09-27 21:07:28 -0400

    initial commit

---
+++ Spotz/SpotzApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotzApp.swift
+//  Spotz
+//
+//  Created by Michael Vilabrera on 9/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
