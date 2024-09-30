f7021227620a91e359604fafd6e958b1248d7ea9
FullMetalFist <m.vilabrera@gmail.com>
2024-09-30 10:49:25 -0400

    Creating and combining views

--- Spotz/ContentView.swift
+++ Spotz/ContentView.swift
@@ -10,12 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapView()
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("3rd Ave Playground")
+                    .font(.title)
+                HStack {
+                    Text("Francis Lewis Park")
+                        
+                    Spacer()
+                    Text("New York")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                Text("About Francis Lewis Park")
+                Text("Descriptive Lorem Ipsum")
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
