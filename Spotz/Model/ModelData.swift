d9a73f568078258b660e2703710e3082fa77e5c7
FullMetalFist <m.vilabrera@gmail.com>
2024-10-06 12:59:41 -0400

    add animations with HikeView & hikeData.json

--- Spotz/Model/ModelData.swift
+++ Spotz/Model/ModelData.swift
@@ -10,6 +10,7 @@ import Foundation
 @Observable
 class ModelData {
     var spotz: [Spot] = load("parkData.json")
+    var hikes: [Hike] = load("hikeData.json")
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
